34dfe464aaacd5e1d53949059b407612cd29b8d3
Jacob Zufall <jacobzufall@gmail.com>
2022-10-04 08:46:03 -0400

    Update script.lua
    Fixed styling.

--- script.lua
+++ script.lua
@@ -17,7 +17,7 @@ local fov = 20 -- You can tweak this, but it may result in some distortion. 20 g
  
 
 --[Functions]
-local function zoomCam(direction) -- Zooms the camera inside the parameters defined above.
+local function ZoomCam(direction) -- Zooms the camera inside the parameters defined above.
     if direction == -1 then
         if camDepth ~= maxZoom then
             camDepth += 1
@@ -29,7 +29,7 @@ local function zoomCam(direction) -- Zooms the camera inside the parameters defi
     end    
 end
  
-local function updateCam() -- This constantly runs (see below) so the camera stays at one position.
+local function UpdateCam() -- This constantly runs (see below) so the camera stays at one position.
     local character = player.Character
     
     if character then
@@ -48,9 +48,9 @@ local function updateCam() -- This constantly runs (see below) so the camera sta
 end 
 
 --[Interaction]
-RunService:BindToRenderStep("Isometric Camera", Enum.RenderPriority.Camera.Value + 1, updateCam) -- Basically renders every frame how I want.
+RunService:BindToRenderStep("Isometric Camera", Enum.RenderPriority.Camera.Value + 1, UpdateCam) -- Basically renders every frame how I want.
 UserInputService.InputChanged:Connect(function(input) -- Waits for the user to input on the scrollwheel and runs the function based on the direction of the wheel.
     if input.UserInputType == Enum.UserInputType.MouseWheel then
-        zoomCam(input.Position.Z)
+        ZoomCam(input.Position.Z)
     end
 end)
